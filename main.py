08723aab050dac36e1eab6b4bf78cb6acdf9ba0a
Hinamizawa <themoonbunnys@gmail.com>
2022-03-01 10:54:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,25 @@ window.config(background="grey")
 # photo
 photo = PhotoImage(file='views/ft1.png')
 
+
+def click():
+    print("You click the button!")
+
+
 # test
-label = Label(window, text="Hello", font=(
+label = Label(window, text="SMART-OBC File Comparison", font=(
     'Arial', 40, 'bold'), fg='pink', bg="black",  # font-text
     relief=RAISED, bd=10, padx=20, pady=20,  # Border
     image=photo, compound='bottom')  # image
 
 label.pack()
-#label.place(x=0, y=0)
+# label.place(x=0, y=0)
 
-
+# button
+button = Button(window, text="Submit",
+                command=click,
+                font=("Comic Sans", 30), fg="pink", bg="black",
+                activeforeground="pink",  # button color flash removed
+                activebackground="black")
+button.pack()
 window.mainloop()
