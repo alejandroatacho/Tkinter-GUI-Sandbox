0b931b6f0f2fcab2759f5d5573842d1de295ea41
Hinamizawa <themoonbunnys@gmail.com>
2022-03-01 10:39:33 -0400

    update GUI

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
-#main GUI
-window = Tk()#instantiate an instance of a window
+# main GUI
+window = Tk()  # instantiate an instance of a window
 window.geometry("420x420")
 window.title("GUI for app")
-#test
-label = Label(window,text="Hello")
-label.pack()
-#label.place(x=0, y=0)
-#foto
+
+# foto
 icon = PhotoImage(file='views/icon1.png')
 window.iconphoto(True, icon)
 window.config(background="grey")
+# photo
+photo = PhotoImage(file='views/ft1.png')
 
+# test
+label = Label(window, text="Hello", font=(
+    'Arial', 40, 'bold'), fg='pink', bg="black",  # font-text
+    relief=RAISED, bd=10, padx=20, pady=20,  # Border
+    image=photo, compound='bottom')  # image
+
+label.pack()
+#label.place(x=0, y=0)
 
 
 window.mainloop()
